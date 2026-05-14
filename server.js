af47fdfbc09e267a7ba9075c2b733f12bb393956
Abhay12m <mohodabhay.am@gmail.com>
2026-05-14 12:42:19 +0530

    docker file added

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use("/api", require("./routes/routes"));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("✅ Server running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
